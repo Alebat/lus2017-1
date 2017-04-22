06af2509c089bfd97178ba22329fc93d1454cd60
Alessandro <Alebat@users.noreply.github.com>
2017-04-22 12:36:18 +0200

    Update history.2grams.sh

--- history.2grams.sh
+++ history.2grams.sh
@@ -33,4 +33,4 @@ done > prediction.raw.csv
 cat prediction.raw.csv | sed 's/^[0-9]*\t[0-9\.]*$/-X-/' | sed -r 's/^[0-9\ ]+$/-X-/' | sed '/<eps>/d' | cut -f 4 | paste NLSPARQL.test.data - | sed 's/.*-X-//' > prediction.csv
 
 # Evaluation
-cat prediction.csv | tr '\t' ' ' | ./conlleval.pl -l > eval1.txt
+cat prediction.csv | tr '\t' ' ' | ./conlleval.pl -l > eval.txt
